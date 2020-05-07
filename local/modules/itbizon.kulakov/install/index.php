68bac321b52ef963addc227cd2617593489922e9
Dmitriy <dmitriy.kulakov.000@gmail.com>
2020-05-07 16:06:34 +0500

    Added test module

--- local/modules/itbizon.kulakov/install/index.php
+++ local/modules/itbizon.kulakov/install/index.php
@@ -1,1 +1,51 @@
 <?php
+
+use Bitrix\Main\Application;
+use Bitrix\Main\ModuleManager;
+
+class TestModule extends CModule
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->MODULE_ID = 'itbizon.kulakov';
+        $this->MODULE_NAME = '[BizON] Тестовый модуль';
+        $this->MODULE_DESCRIPTION = 'Модуль от Кулакова';
+        $this->PARTNER_NAME = 'BizON';
+        $this->PARTNER_URI = 'https://itbizon.com';
+    }
+
+    /**
+     * Install
+     */
+    public function DoInstall()
+    {
+        if(!ModuleManager::isModuleInstalled($this->MODULE_ID))
+        {
+            CAdminMessage::ShowNote('Тестовый модуль установлен');
+        } else
+        {
+            CAdminMessage::ShowNote('Ошибка установки тестого модуля');
+        }
+
+        ModuleManager::registerModule($this->MODULE_ID);
+    }
+
+    /**
+     * Uninstall
+     */
+    public function DoUninstall()
+    {
+        if(ModuleManager::isModuleInstalled($this->MODULE_ID))
+        {
+            CAdminMessage::ShowNote('Тестовый модуль удален');
+        } else
+        {
+            CAdminMessage::ShowNote('Ошибка удаления тестого модуля');
+        }
+
+        ModuleManager::unRegisterModule($this->MODULE_ID);
+    }
+}
