24c8b12748429ec668d60dcc692aa74526a70863
Denis Savich <deonisiu@ya.ru>
2020-07-06 14:13:11 +0700

    Добавлена функция i_show в php_interface

---
+++ local/php_interface/include/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function i_show($obj)
+{
+    echo '<pre>'.print_r($obj, true).'</pre>';
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -8,3 +8,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/event_handlers.php')) {
     require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/event_handlers.php';
 }
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/functions.php')) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/functions.php';
+}
