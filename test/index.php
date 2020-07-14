00d71a6eb964cdaa9035544c2996a8764effcb58
Komyagin Pavel <komyagin.p@gmail.com>
2020-07-14 18:02:16 +0300

    Класс для работы с чек-листом

--- test/index.php
+++ test/index.php
@@ -1,10 +1,19 @@
 <?php
+
+use Bitrix\Main\Loader;
+use Bizon\Main\Tasks\CheckItem;
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
 
 try
 {
+    if(!Loader::includeModule('bizon.main'))
+        throw new Exception('Ошибка подключения модуля bizon.main');
 
+    //$list = CheckItem::getList($taskId);
+    //CheckItem::onTaskUpdate(1, 2, 3);
+    //echo '<pre>'.print_r($list, true).'</pre>';
 }
 catch(Exception $e)
 {
