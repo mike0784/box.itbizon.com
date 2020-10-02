c53abc0eab8c10a1be7cfb5b6f59ef8f5bef86c1
DeadSky <deadsky.1136@gmail.com>
2020-10-02 14:40:57 +0300

    fix (task #4181 test/kalinin) Исправления вывода.

--- local/test/kalinin/index.php
+++ local/test/kalinin/index.php
@@ -5,17 +5,11 @@ use \Bitrix\Main\Loader;
 require($_SERVER['DOCUMENT_ROOT'] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Проверка тестового модуля");
 
-try
-{
-    if (!Loader::includeModule('itbizon.kalinin'))
-        throw new Exception('Ошибка подключения модуля itbizon.kalinin');
 
+if (CModule::includeModule('itbizon.kalinin'))
+{
     itbizon_kalinin::SayHello("BizON");
+}
 
-}
-catch (Exception $e)
-{
-    echo $e->getMessage();
-}
 
 require($_SERVER['DOCUMENT_ROOT'] . "/bitrix/footer.php");
