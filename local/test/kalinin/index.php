e709822184b8c463e446ed97b1f965d71238148b
DeadSky <deadsky.1136@gmail.com>
2020-10-02 15:40:04 +0300

    fix (--- itbizon.kalinin) Мелкие исправления.

--- local/test/kalinin/index.php
+++ local/test/kalinin/index.php
@@ -1,12 +1,11 @@
 <?php
 
-use \Bitrix\Main\Loader;
 
 require($_SERVER['DOCUMENT_ROOT'] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Проверка тестового модуля");
 
 
-if (CModule::includeModule('itbizon.kalinin'))
+if (CModule::IncludeModule('itbizon.kalinin'))
 {
     itbizon_kalinin::SayHello("BizON");
 }
