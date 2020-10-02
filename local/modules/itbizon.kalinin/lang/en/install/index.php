d6ede1fff6a7b71daec6b4985cbe36e6dc30592c
DeadSky <deadsky.1136@gmail.com>
2020-10-02 15:55:35 +0300

    dev (task #4181 itbizon.kalinin) Добавление локализации 'en'

---
+++ local/modules/itbizon.kalinin/lang/en/install/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['INSTALL'] = "Installation itbizon.kalinin";
+$MESS['UNINSTALL'] = "Uninstallation itbizon.kalinin";
+$MESS['M_NAME'] = "[BizON] Test module (DS)";
+$MESS['M_DESC'] = "It's just a test (DS)";
