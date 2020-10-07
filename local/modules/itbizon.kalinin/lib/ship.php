503e0e4475160732140f0e5773c49b85ca152829
DeadSky <deadsky.1136@gmail.com>
2020-10-07 14:49:55 +0300

    dev (task #4181 itbizon.kalinin) Добавлено наследование сущностей

---
+++ local/modules/itbizon.kalinin/lib/ship.php
@@ -0,0 +1,7 @@
+<?php
+
+use \Itbizon\Kalinin\Lib\Model\EO_Ship;
+
+class Ship extends EO_Ship
+{
+}
---
+++ local/modules/itbizon.kalinin/lib/station.php
@@ -0,0 +1,7 @@
+<?php
+
+use \Itbizon\Kalinin\Lib\Model\EO_Station;
+
+class Station extends EO_Station
+{
+}
