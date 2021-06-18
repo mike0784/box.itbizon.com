9d9339e9f595323f6223d3a9711dbf2906dc0176
Vlad Orlov <orlov1@yandex.ru>
2021-06-18 14:54:31 +0300

    компонент service.notify.settings, доработана локализация

--- local/components/itbizon/service.notify.settings/templates/.default/template.php
+++ local/components/itbizon/service.notify.settings/templates/.default/template.php
@@ -36,15 +36,15 @@ $toUsers = $component->toUsers;
         <input type='hidden' name='select_from_user' value='true'>
         <div class="card-body">
         <div class="form-group">
-            <h5 class="card-title">Загрузить настройки</h5>
+            <h5 class="card-title"><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.LOAD_TITLE')?></h5>
             <select style="option:first {color: #999;}" name="FROM_USER_ID" id="FROM_USER_ID" class="form-control">
-                <option style="color: grey" value="" disabled selected>Выберите пользователя</option>
+                <option style="color: grey" value="" disabled selected><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.SELECT_USER')?></option>
             <? foreach ($usersList as $key => $user): ?>
-                <option value="<?= $user['ID'] ?>"><?= $user['LAST_NAME']." ". $user['NAME']." ". $user['SECOND_NAME'] ?></option>
+                <option value="<?= $user['ID'] ?>" <?= ($user['ID'] == $fromUser)?'selected':''  ?> ><?= $user['LAST_NAME']." ". $user['NAME']." ". $user['SECOND_NAME'] ?></option>
             <? endforeach; ?>
             </select>
         </div>
-        <button class='btn btn-primary' type='submit'>Загрузить</button>
+        <button class='btn btn-primary' type='submit'><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.LOAD_BUTTON')?></button>
         </div>
         
     </form>
@@ -55,14 +55,14 @@ $toUsers = $component->toUsers;
         <div class="card" style='width:900px;'>
             <div class="card-body">
                 <div class="form-group">
-                    <h5 class="card-title">Сохранить настройки</h5>
+                    <h5 class="card-title"><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.SAVE_TITLE')?></h5>
                     <select name="TO_USER_ID[]" id="TO_USER_ID" class="form-control" multiple>
                         <? foreach ($usersList as $key => $user): ?>
-                            <option value="<?= $user['ID'] ?>" <?= (in_array($user['ID'], $toUsers))?'selected':''  ?>    ><?= $user['LAST_NAME']." ". $user['NAME']." ".$user['SECOND_NAME'] ?></option>
+                            <option value="<?= $user['ID'] ?>" <?= (in_array($user['ID'], $toUsers))?'selected':''  ?> ><?= $user['LAST_NAME']." ". $user['NAME']." ".$user['SECOND_NAME'] ?></option>
                         <? endforeach; ?>
                     </select>
                 </div>
-                <button class='btn btn-primary' type='submit'>Сохранить</button>
+                <button class='btn btn-primary' type='submit'><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.SAVE_BUTTON')?></button>
             </div>
         </div>
 
@@ -71,7 +71,12 @@ $toUsers = $component->toUsers;
     <input type='hidden' name='form_notify_table' value='true'>
     <div style='display: block;' id='bx-messenger-settings-content-notify' class='bx-messenger-settings-content' >
     <table  class='bx-messenger-settings-table bx-messenger-settings-table-style-notify bx-messenger-settings-table-extra' >
-    <tr><th></th><th>Сайт и<br>приложения</th><th>Электронная почта</th><th>Push уведомления</th></tr>
+    <tr>
+        <th></th>
+        <th><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.TABLE.SITE')?></th>
+        <th><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.TABLE.EMAIL')?></th>
+        <th><?= Loc::getMessage('ITB_SERVICE.NOTIFY.SETTINGS.TABLE.PUSH')?></th>
+    </tr>
 
     <?
     foreach($res as $row_num => $row){
@@ -80,7 +85,7 @@ $toUsers = $component->toUsers;
         } elseif  ($row['type'] == 'data') {
             echo "<tr><td>".$row['title']."</td> <td>".$row['site']."</td> <td>".$row['email']."</td> <td>".$row['push']."</td> </tr>";
         } else
-            echo "<tr><td> DEBUG".print_r($row, True)."</td></tr>";
+            echo "<tr><td> DEBUG".print_r($row, True)."</td></tr>"; // fixme
     }
     ?>
     </table>
