89d0d57ef781cc7838d83a795a7f2a06e04b441a
Komyagin Pavel <komyagin.p@gmail.com>
2022-02-21 23:35:23 +0300

    Баловство с API

--- local/modules/itbizon.api/lib/controller/base.php
+++ local/modules/itbizon.api/lib/controller/base.php
@@ -3,14 +3,29 @@
 namespace Itbizon\API\Controller;
 
 use Bitrix\Main\Engine\Controller;
+use Bitrix\Main\Error;
 use Itbizon\API\ActionFilter\Auth;
 
-abstract class Base extends Controller
+class Base extends Controller
 {
+    public function configureActions()
+    {
+        return [
+            'notfound' => [
+                'prefilters' => []
+            ]
+        ];
+    }
+
     protected function getDefaultPreFilters()
     {
         return [
             new Auth()
         ];
+    }
+
+    public function notfoundAction()
+    {
+        $this->addError(new Error('No route'));
     }
 }
--- local/routes/api.php
+++ local/routes/api.php
@@ -2,15 +2,22 @@
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Routing\RoutingConfigurator;
+use Itbizon\API\Controller\Base;
 use Itbizon\API\Controller\Test;
 
 return function (RoutingConfigurator $routes) {
     if (Loader::includeModule('itbizon.api')) {
         $routes->prefix('api')->name('api')->group(function (RoutingConfigurator $routes) {
             $routes->prefix('v1')->name('v1')->group(function (RoutingConfigurator $routes) {
-                // path /api/v1/test
+
+                // api/v1/test
                 $routes->any('test',  [Test::class, 'index'])->methods(['GET', 'POST']);
+
+                //Default route - not found
+                $routes->any('{request}', [Base::class, 'notfound'])->where('request', '.*');
             });
+            //Default route - not found
+            $routes->any('{request}', [Base::class, 'notfound'])->where('request', '.*');
         });
     }
 };
